87b37f7babbd69a50de4a8421ded71c6ba5b0d4b
kylesteinmann <105984447+kylesteinmann@users.noreply.github.com>
2022-09-03 23:11:33 -0500

    Completed challenges for September 8th

---
+++ Array Challenge.js
@@ -0,0 +1,16 @@
+ const brothers = [
+    {
+        firstName: "Johnny",
+        Weight: 150
+    },
+    {
+        firstName: "Jimmy",
+        Weight: 160
+    },
+    {
+        firstName: "James",
+        Weight: 170
+    }
+]
+
+console.log(`We are three brothers: ${brothers[0].firstName}, ${brothers[1].firstName}, ${brothers[2].firstName}`)
---
+++ Object Challenge.js
@@ -0,0 +1,13 @@
+let person ={
+    firstName: "john",
+    age: 35,
+    favoriteIceCream: "strawberry",
+    vehicle: {
+        model:"hyundia"
+    }
+};
+
+console.log(`My name is ${person.firstName}.`);
+console.log(`My age is ${person.age}.`);
+console.log(`My favorite ice cream type is ${person.favoriteIceCream}.`);
+console.log(`I own a ${person.vehicle.model}`);
